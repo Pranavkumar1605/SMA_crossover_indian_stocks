4a317123a08eadea4cedb42745e2415730a21cfa
Pranavkumar1605 <Pranavkumar1605@gmail.com>
2026-01-18 19:45:53 +0530

    strategy.py finished and a notebook created for analysis

--- src/strategy.py
+++ src/strategy.py
@@ -13,20 +13,44 @@ def calculate_sma(df, short_window=20, long_window=50):
 
     return df_copy
 
+
 def gnerate_signals(df):
-    
+
     df_copy = df.copy()
-    
+
     df_copy["Signal"] = 0
-    
-    df_copy["Position"] = np.where(df_copy['SMA_20'] > df_copy['SMA_50'],1,0)
-    df_copy['Signal'] = df_copy['Position'].diff()
-    
-    buy_signals = len(df_copy[df_copy['Signal'] == 1])
-    sell_signals = len(df_copy[df_copy['Signal'] == -1])
-    
+
+    df_copy["Position"] = np.where(df_copy["SMA_20"] > df_copy["SMA_50"], 1, 0)
+    df_copy["Signal"] = df_copy["Position"].diff()
+
+    buy_signals = len(df_copy[df_copy["Signal"] == 1])
+    sell_signals = len(df_copy[df_copy["Signal"] == -1])
+
     print(f"Buy signals:{buy_signals}")
     print(f"Sell signals:{sell_signals}")
-    
+
     return df_copy
 
+
+if __name__ == "__main__":
+
+    import sys
+
+    sys.path.append(".")
+    from src.data_loader import download_stock_data
+
+    print("testing strategy module")
+
+    ticker = "RELIANCE.NS"
+    df = download_stock_data(ticker, period="5y", interval="1d")
+
+    if df is not None:
+
+        df_SMA = calculate_sma(df, short_window=20, long_window=50)
+
+        df_signals = gnerate_signals(df_SMA)
+
+        print("Example of generated signals:")
+        print(df_signals[["Close", "SMA_20", "SMA_50", "Position", "Signal"]].tail(10))
+
+        print("module working correctly")
